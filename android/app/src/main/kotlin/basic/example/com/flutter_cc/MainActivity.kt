6ed7da847db4ad5eb5187ad0c933cfbb0de59e8c
Rana Ranvijay Singh <rana.singh@globant.com>
2020-01-17 12:13:13 +0530

    First commit

---
+++ android/app/src/main/kotlin/basic/example/com/flutter_cc/MainActivity.kt
@@ -0,0 +1,12 @@
+package basic.example.com.flutter_cc
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
